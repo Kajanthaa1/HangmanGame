e81968809d611c359ff3f7cffde6bf8e84bf76af
sangeethasanthiralingam <sangeethasanthiralingam@gmail.com>
2024-01-17 09:11:13 +0530

    done

--- database/migrations/2024_01_16_114250_attempts_table.php
+++ database/migrations/2024_01_16_114250_attempts_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
     {
         Schema::create('attempts', function (Blueprint $table) {
             $table->string('time');
+            
             $table->bigInteger('match_id')->unsigned()->index()->nullable();
             $table->foreign('match_id')->references('id')->on('match')->onDelete('cascade');
         });
